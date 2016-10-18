7727e7e6f993b9df0ac8f5d82e290cb7b48b65c1
hichem <htebbouche@gmail.com>
2016-10-18 10:12:36 +0200

    ajout commentaire

--- Fil_Rouge_ForumDLL/Sujet.cs
+++ Fil_Rouge_ForumDLL/Sujet.cs
@@ -7,18 +7,29 @@ using System.Threading.Tasks;
 
 namespace Fil_Rouge_Forum
 {
+    /// <summary>
+    /// La classe sujet, permet de voir tous les sujets d'une rubrique
+    /// Pemet de voir les reponses, concernant un sujet
+    /// Elle comporte un identifiant, titre du sujet, la description, la rubrique et les reponses
+    /// </summary>
     class Sujet
     {
 
-
         #region "Property et attributs"
-        //private List<Sujet> _Sujets = new List<Sujet>();
+        /// <summary>
+        /// La liste de reponse, concernant un sujet
+        /// </summary>
+        private List<Reponse> _Reponses;
 
-        //public List<Sujet> Sujets
-        //{
-        //    get { return _Sujets; }
-        //}
+        public List<Reponse> Reponses
+        {
+            get { return _Reponses; }
+            set { _Reponses = value; }
+        }
 
+        /// <summary>
+        /// l'identifiant du sujet
+        /// </summary>
         private int _IdSujet;
 
         public int IdSujet
@@ -27,6 +38,9 @@ namespace Fil_Rouge_Forum
             set { _IdSujet = value; }
         }
 
+        /// <summary>
+        /// La description d'un sujet
+        /// </summary>
         private string _Desc;
 
         public string Desc
@@ -35,14 +49,20 @@ namespace Fil_Rouge_Forum
             set { _Desc = value; }
         }
 
-        private DateTime  _Datecreation;
+        /// <summary>
+        /// La date de la création du sujet
+        /// </summary>
+        private DateTime _Datecreation;
 
-        public DateTime  Datecreation
+        public DateTime Datecreation
         {
             get { return _Datecreation; }
             set { _Datecreation = value; }
         }
 
+        /// <summary>
+        /// La rubrique à la quelle appartient le sujet
+        /// </summary>
         private Categorie _Cat;
 
         public Categorie Cat
@@ -51,6 +71,9 @@ namespace Fil_Rouge_Forum
             set { _Cat = value; }
         }
 
+        /// <summary>
+        /// Le titre du sujet
+        /// </summary>
         private string _Titre;
 
         public string Titre
@@ -59,36 +82,41 @@ namespace Fil_Rouge_Forum
             set { _Titre = value; }
         }
 
-        private Utilisateur _Auteur;
-
-        public Utilisateur Auteur
-        {
-            get { return _Auteur; }
-            set { _Auteur = value; }
-        }
-
-
-
-
-
         #endregion
+
         #region "Constructeurs"
 
         /// <summary>
-        /// 
+        /// Constructeur d'un nouveau sujet, sans reponse
         /// </summary>
-        /// <param name="idsujet"></param>
-        /// <param name="desc"></param>
-        /// <param name="categorie"></param>
-        public Sujet(int Idsujet, string Desc, Categorie Cat, Utilisateur Auteur)
+        /// <param name="id">L'identifiant du sujet</param>
+        /// <param name="description">La description du sujet</param>
+        /// <param name="titre">Le titre du sujet</param>
+        /// <param name="categorie">La rubrique à la quelle appartient le sujet</param>
+        public Sujet(int id, string description, string titre, Categorie categorie)
         {
-            this._IdSujet = IdSujet;
-            this._Cat = Cat;
-            this._Desc = Desc;
-            this._Datecreation = Datecreation;
-            this._Titre = Titre;
-            this._Auteur = Auteur;
+            this.IdSujet = id;
+            this.Desc = description;
+            this.Titre = titre;
+            this.Cat = categorie;
+            this.Reponses = new List<Reponse>();
+            this.Datecreation = DateTime.Now;
         }
+
+        /// <summary>
+        /// Constructeur d'un sujet, avec des reponse, on fait appel au constructeur du sujet sans reponse
+        /// </summary>
+        /// <param name="id">L'identifiant du sujet</param>
+        /// <param name="description">La description du sujet</param>
+        /// <param name="titre">Le titre du sujet</param>
+        /// <param name="categorie">La rubrique du sujet</param>
+        /// <param name="Reponses">Les reponses du sujet</param>
+        public Sujet(int id, string description, string titre, Categorie categorie, List<Reponse> Reponses) : this(id, description, titre, categorie)
+        {
+            _Reponses = Reponses;
+        }
+
+
         #endregion
         #region "Methodes"
         #endregion
--- Fil_Rouge_ForumDLL/Utilisateur.cs
+++ Fil_Rouge_ForumDLL/Utilisateur.cs
@@ -6,31 +6,76 @@ using System.Threading.Tasks;
 
 namespace Fil_Rouge_ForumDLL
 {
+    /// <summary>
+    /// La classe Utilisateur permet à un utilisateur de s'identifier avec 
+    /// son login (nom) et son mot de passe
+    /// </summary>
     public class Utilisateur
     {
 
         #region "Property et attributs"
-        private int _Login;
+        /// <summary>
+        /// Le login (nom) de l'utilisateur
+        /// </summary>
+        private string _Login;
 
-        public int Login
+        public string Login
         {
             get { return _Login; }
             set { _Login = value; }
         }
 
-        private int _Password;
+        /// <summary>
+        /// Le mot de passe de l'utilisateur
+        /// </summary>
+        private string _Password;
 
-        public int Password
+        public string Password
         {
             get { return _Password; }
             set { _Password = value; }
         }
 
+        /// <summary>
+        /// L'identifiant de l'utilisateur
+        /// </summary>
+        private int _IdUser;
 
+        public int UdUser
+        {
+            get { return _IdUser; }
+            set { _IdUser = value; }
+        }
 
+        /// <summary>
+        /// Le role de l'utilisateur, soit il est modérateur ou un utilisateur logged
+        /// </summary>
+        private bool _Role;
+
+        public bool Role
+        {
+            get { return _Role; }
+            set { _Role = value; }
+        }
 
         #endregion
+
         #region "Constructeurs"
+
+        /// <summary>
+        /// constructeur d'un utilisateur
+        /// </summary>
+        /// <param name="iduser">L'identifiant de l'utilisateur</param>
+        /// <param name="login">Le login de l'utilisateur</param>
+        /// <param name="password">Le mot de passe de l'utilisateur</param>
+        /// <param name="role">Le role attribut à un utilisateur</param>
+        public Utilisateur(int iduser, string login, string password, bool role)
+        {
+            this._IdUser = iduser;
+            this.Login = login;
+            this.Password = password;
+            this.Role = role;
+        }
         #endregion
         #region "Methodes"
         #endregion
